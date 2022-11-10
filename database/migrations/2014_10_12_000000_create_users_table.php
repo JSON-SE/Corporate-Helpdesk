839547f483bec8e25c47e1dfc705df39ce2e9b66
Jayson R. Alanano <json.alanano@gmail.com>
2022-11-10 10:48:27 +0800

    User Middlename Hotfix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstName');
-            $table->string('middleName')->nullable;
+            $table->string('middleName')->nullable();
             $table->string('lastName');
             $table->string('initials');
             $table->foreignId('office_id')->constrained();
