59c60d34b6587fd8212519ea3279bab7bef34460
Jayson R. Alanano <json.alanano@gmail.com>
2022-12-19 11:26:19 +0800

    Added tally board number of generated tickets feature

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Inertia\Inertia;
 use App\Models\Office;
 use App\Models\Status;
@@ -11,9 +12,126 @@ use Illuminate\Support\Facades\Request;
 
 class DashboardController extends Controller
 {
+    public function weeklyTicketCount()
+    {
+        // Retrieve the current week's ticket count
+        $current_weekly_count = Ticket::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
+
+        // Retrieve the previous week's ticket count
+        $previous_weekly_count = Ticket::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()])->count();
+
+        // Calculate the percentage change in ticket count between the two weeks
+        if ($previous_weekly_count > 0) {
+            $percentage_change = ($current_weekly_count - $previous_weekly_count) / $previous_weekly_count * 100;
+        } else {
+            $percentage_change = 0;
+        }
+
+        // Retrieve the total number of tickets created
+        $total_ticket_count = Ticket::count();
+
+        // Return if the generated ticket has increase or decrease status
+        if ($current_weekly_count > $previous_weekly_count) {
+            $changeType = 'increase';
+        } else {
+            $changeType = 'decrease';
+        }
+
+        // Return the current weekly count, percentage change, and total ticket count
+        return [
+            'name' => 'Weekly Generated Tickets',
+            'stat' => $current_weekly_count,
+            'previousStat' => $previous_weekly_count,
+            'change' => $percentage_change,
+            'changeType' => $changeType
+        ];
+    }
+
+    public function monthlyTicketCount()
+    {
+        // Retrieve the current month's ticket count
+        $current_monthly_count = Ticket::whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
+
+        // Retrieve the previous month's ticket count
+        $previous_monthly_count = Ticket::whereBetween('created_at', [Carbon::now()->subMonth()->startOfMonth(), Carbon::now()->subMonth()->endOfMonth()])->count();
+
+        // Calculate the percentage change in ticket count between the two months
+        if ($previous_monthly_count > 0) {
+            $percentage_change = ($current_monthly_count - $previous_monthly_count) / $previous_monthly_count * 100;
+        } else {
+            $percentage_change = 0;
+        }
+
+        // Retrieve the total number of tickets created
+        $total_ticket_count = Ticket::count();
+
+        // Return if the generated ticket has increase or decrease status
+        if ($current_monthly_count > $previous_monthly_count) {
+            $changeType = 'increase';
+        } else {
+            $changeType = 'decrease';
+        }
+
+        // Return the current weekly count, percentage change, and total ticket count
+        return [
+            'name' => 'Monthly Generated Tickets',
+            'stat' => $current_monthly_count,
+            'previousStat' => $previous_monthly_count,
+            'change' => $percentage_change,
+            'changeType' => $changeType
+        ];
+    }
+
+    public function yearlyTicketCount()
+    {
+        // Retrieve the current year's ticket count
+        $current_yearly_count = Ticket::whereYear('created_at', Carbon::now()->year)->count();
+
+        // Retrieve the ticket count for the past number of months
+        $previous_yearly_count = Ticket::whereYear('created_at', Carbon::now()->year)
+                                                ->whereMonth('created_at', '<', Carbon::now()->month)
+                                                ->count();
+
+        // Calculate the percentage change in ticket count between the two time periods
+        if ($previous_yearly_count > 0) {
+            $percentage_change = ($current_yearly_count - $previous_yearly_count) / $previous_yearly_count * 100;
+        } else {
+            $percentage_change = 0;
+        }
+
+        // Retrieve the total number of tickets created
+        $total_ticket_count = Ticket::count();
+
+        // Return if the generated ticket has increase or decrease status
+        if ($current_yearly_count > $previous_yearly_count) {
+            $changeType = 'increase';
+        } else {
+            $changeType = 'decrease';
+        }
+
+        // Return the current weekly count, percentage change, and total ticket count
+        return [
+            'name' => 'Yearly Generated Tickets',
+            'stat' => $current_yearly_count,
+            'previousStat' => $previous_yearly_count,
+            'change' => $percentage_change,
+            'changeType' => $changeType
+        ];
+    }
+
     public function index()
     {
+        $weeklyTicketCount = DashboardController::weeklyTicketCount();
+        $monthlyTicketCount = DashboardController::monthlyTicketCount();
+        $yearlyTicketCount = DashboardController::yearlyTicketCount();
+        $ticketCount = [
+            $weeklyTicketCount,
+            $monthlyTicketCount,
+            $yearlyTicketCount
+        ];
+
         return Inertia::render('Dashboard', [
+            'ticket_counter' => $ticketCount,
             'tickets' => Ticket::query()
             ->when(Request::input('search'), function ($query, $requestor) {
                 $query->where('requestor', 'like', "%{$requestor}%")
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,12 +7,15 @@ import {
     ArrowPathIcon as ArrowPath,
     ChevronLeftIcon,
     ChevronRightIcon,
+    ArrowDownIcon,
+    ArrowUpIcon,
 } from "@heroicons/vue/20/solid";
 import { Link } from "@inertiajs/inertia-vue3";
 import { moment } from "moment";
 import { Inertia } from "@inertiajs/inertia";
 
 const props = defineProps({
+    ticket_counter: Object,
     tickets: Object,
     categories: Object,
     offices: Object,
@@ -126,6 +129,68 @@ function acceptTicket(id) {
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <!-- Ticket counter -->
+                <div class="mb-5">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">
+                        Last 30 days
+                    </h3>
+                    <dl
+                        class="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x"
+                    >
+                        <div
+                            v-for="item in ticket_counter"
+                            :key="item.name"
+                            class="px-4 py-5 sm:p-6"
+                        >
+                            <dt class="text-base font-normal text-gray-900">
+                                {{ item.name }}
+                            </dt>
+                            <dd
+                                class="mt-1 flex items-baseline justify-between md:block lg:flex"
+                            >
+                                <div
+                                    class="flex items-baseline text-2xl font-semibold text-indigo-600"
+                                >
+                                    {{ item.stat }}
+                                    <span
+                                        class="ml-2 text-sm font-medium text-gray-500"
+                                        >from {{ item.previousStat }}</span
+                                    >
+                                </div>
+
+                                <div
+                                    :class="[
+                                        item.changeType === 'increase'
+                                            ? 'bg-green-100 text-green-800'
+                                            : 'bg-red-100 text-red-800',
+                                        'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0',
+                                    ]"
+                                >
+                                    <ArrowUpIcon
+                                        v-if="item.changeType === 'increase'"
+                                        class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                                        aria-hidden="true"
+                                    />
+                                    <ArrowDownIcon
+                                        v-else
+                                        class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                                        aria-hidden="true"
+                                    />
+                                    <span class="sr-only">
+                                        {{
+                                            item.changeType === "increase"
+                                                ? "Increased"
+                                                : "Decreased"
+                                        }}
+                                        by
+                                    </span>
+                                    {{ item.change }}%
+                                </div>
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+                <!-- end Ticket counter -->
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="px-4 sm:px-6 lg:px-8 mt-6 pb-8">
                         <div class="sm:flex sm:items-center">
