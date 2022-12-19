211885ad60ef1fce81da148096261cd51b8a6298
Jayson R. Alanano <json.alanano@gmail.com>
2022-12-19 17:18:45 +0800

    Working Notification Button show and hide

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,10 +6,19 @@ import DropdownLink from "@/Components/DropdownLink.vue";
 import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/inertia-vue3";
-import { CheckCircleIcon, XMarkIcon } from "@heroicons/vue/20/solid";
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
+import { CheckCircleIcon } from "@heroicons/vue/20/solid";
+import { BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 
 const showingNavigationDropdown = ref(false);
 const show = ref(true);
+const open = ref(false);
 </script>
 
 <template>
@@ -193,7 +202,15 @@ const show = ref(true);
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
+                            <button
+                                @click="open = !open"
+                                type="button"
+                                class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                <span class="sr-only">View notifications</span>
+                                <BellIcon class="h-6 w-6" aria-hidden="true" />
+                            </button>
+                            <div class="ml-1 relative">
                                 <Dropdown align="right" width="48">
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
@@ -328,6 +345,100 @@ const show = ref(true);
 
             <!-- Page Content -->
             <main>
+                <TransitionRoot as="template" :show="open">
+                    <Dialog
+                        as="div"
+                        class="relative z-10"
+                        @close="open = false"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="ease-in-out duration-500"
+                            enter-from="opacity-0"
+                            enter-to="opacity-100"
+                            leave="ease-in-out duration-500"
+                            leave-from="opacity-100"
+                            leave-to="opacity-0"
+                        >
+                            <div
+                                class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                            />
+                        </TransitionChild>
+
+                        <div class="fixed inset-0 overflow-hidden">
+                            <div class="absolute inset-0 overflow-hidden">
+                                <div
+                                    class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
+                                >
+                                    <TransitionChild
+                                        as="template"
+                                        enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                        enter-from="translate-x-full"
+                                        enter-to="translate-x-0"
+                                        leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                        leave-from="translate-x-0"
+                                        leave-to="translate-x-full"
+                                    >
+                                        <DialogPanel
+                                            class="pointer-events-auto w-screen max-w-md"
+                                        >
+                                            <div
+                                                class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl"
+                                            >
+                                                <div class="px-4 sm:px-6">
+                                                    <div
+                                                        class="flex items-start justify-between"
+                                                    >
+                                                        <DialogTitle
+                                                            class="text-lg font-medium text-gray-900"
+                                                            >Panel
+                                                            title</DialogTitle
+                                                        >
+                                                        <div
+                                                            class="ml-3 flex h-7 items-center"
+                                                        >
+                                                            <button
+                                                                type="button"
+                                                                class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                                @click="
+                                                                    open = false
+                                                                "
+                                                            >
+                                                                <span
+                                                                    class="sr-only"
+                                                                    >Close
+                                                                    panel</span
+                                                                >
+                                                                <XMarkIcon
+                                                                    class="h-6 w-6"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div
+                                                    class="relative mt-6 flex-1 px-4 sm:px-6"
+                                                >
+                                                    <!-- Replace with your content -->
+                                                    <div
+                                                        class="absolute inset-0 px-4 sm:px-6"
+                                                    >
+                                                        <div
+                                                            class="h-full border-2 border-dashed border-gray-200"
+                                                            aria-hidden="true"
+                                                        />
+                                                    </div>
+                                                    <!-- /End replace -->
+                                                </div>
+                                            </div>
+                                        </DialogPanel>
+                                    </TransitionChild>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog>
+                </TransitionRoot>
                 <!-- Alert Ticket Created -->
                 <div
                     v-if="$page.props.flash.ticketCreated"
